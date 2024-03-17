5d54fbca0bdbacc1685c94ec974969eb0fc36e77
Shamimusman515419 <shamimhossain01617@gmail.com>
2024-03-17 08:50:22 +0600

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,100 @@
+$(".responsive").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  nextArrow: $(".next-arrow"),
+  prevArrow: $(".prev-arrow"),
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+// $('.ourTem').slick({
+//   dots: true,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   centerPadding: "24px",
+//   infinite: true,
+//   nextArrow: false,
+//   prevArrow: false,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+//   responsive: [
+//     {
+//       breakpoint: 1400,
+//       settings: {
+//         slidesToShow: 4,
+//         slidesToScroll: 4,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 654,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }]
+// });
+
+// $('.testimonials').slick({
+
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
+//   prevArrow: false,
+//   nextArrow: false,
+//   autoplay: true,
+//   responsive: [
+//     {
+//       breakpoint: 1400,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 654,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }]
+// });
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      textColor: {
+        primary: "#83b735",
+      },
+      backgroundColor: {
+        secondary: "#83b735",
+      },
+    },
+  },
+  plugins: [],
+};
